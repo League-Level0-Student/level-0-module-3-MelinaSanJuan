c511c782acafe67cb4e3d114943a6c4622d8adce
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2020-02-27 13:27:03 -0800

    Cat Lady IP

--- src/_01_robot_color_chooser/RobotColorChooser.java
+++ src/_01_robot_color_chooser/RobotColorChooser.java
@@ -4,24 +4,45 @@
 package _01_robot_color_chooser;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
+
 public class RobotColorChooser {
+	
+
 	public static void main(String[] args) {
 		//1. Create a new Robot
-		
+		Robot rose = new Robot(); 
 		//3. Ask the user what color they would like the robot to draw
-		
+		String color = JOptionPane.showInputDialog("What color would you like?");
 		//5. Use an if/else statement to set the pen color that the user requested
-
+         if(color.equalsIgnoreCase("red")) {
+		rose.setPenColor(Color.red);
+         }
         //6. If the user doesn’t enter anything, choose a random color
-
+         else {
+        	 rose.setRandomPenColor();
+         }
+          
+		
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
 		
-		//4. Set the pen width to 10
 		
+		//4. Set the pen width to 10
+		rose.setPenWidth(10);
 	    //2. Make the robot draw a shape (this will take more than one line of code)
-
-
+		rose.setSpeed(100);
+       rose.penDown();
+       rose.move(125);
+       rose.turn(90);
+       rose.move(125);
+       rose.turn(90);
+       rose.move(125);
+       rose.turn(90);
+       rose.move(125);
 	}
 }
+
