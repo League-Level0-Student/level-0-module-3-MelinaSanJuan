c66964af4f3f09138ff31d2d4a4d70c5983214d1
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2020-03-05 13:26:42 -0800

    High Low Game

--- src/_02_crazy_cat_lady/CrazyCatLady.java
+++ src/_02_crazy_cat_lady/CrazyCatLady.java
@@ -13,13 +13,20 @@ public class CrazyCatLady {
 		int cats;
 		String answer = JOptionPane.showInputDialog("How many cats do you have?");
 		// 2. Convert their answer into an int
-        cats = Integer.parseInt(answer);
+      cats = Integer.parseInt(answer);
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
-        
+        if(cats >=3) {
+        	System.out.print("You're a Crazy Cat Lady!");
+        }
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
-
+        if(cats<=3&& cats>0) {
+        	playVideo( "https://www.youtube.com/watch?v=YyAuFiIv-V4");
+        	
+        }
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
-		
+		if(cats==0) {
+			playVideo("https://www.youtube.com/watch?v=sxAlWUSkACs");
+		}
 	}
 
 	static void playVideo(String videoURL) {
